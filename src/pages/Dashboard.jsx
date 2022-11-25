3fe72dc291d769e05edd64c516d350c22185d79e
alilopez <ali.lopez37@gmail.com>
2022-11-25 00:33:00 -0600

    [ADD] upload files

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,59 @@
+import {useRef} from 'react'
+import '../assets/styles/dashboard.css'
+
 function Dashboard() {
+    const formData = useRef(null);
+
+    const handleClick = (event)=> {
+      event.preventDefault();
+      const formdata = new FormData(formData.current);
+
+      fetch('http://localhost:3000',{
+        method:'POST',
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "Accept": "application/json",
+          "type": "formData"
+        },
+        body: formdata
+      })
+      .then (response => response.json())
+      .then (data => {
+        console.log(data)
+      })
+      .catch (error => {
+        console.log(error)
+      })
+    }
+     
     return (
+      <>
         <h2>Page Dashboard (private)</h2>
+        <form className="container--formdata" ref={formData}>
+                <div className="row mb-3">
+                    <label htmlFor="name" className="col-sm-2 col-form-label">Nombre</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" id="name" name="name"/>
+                    </div>
+                </div>
+                <div className="row mb-3">
+                    <label htmlFor="direccion" className="col-sm-2 col-form-label">Dirección</label>
+                    <div className="col-sm-10">
+                        <input type="text" className="form-control" id="direccion" name="address"/>
+                    </div>
+                </div>
+
+                <div className="row mb-3">
+                    <label htmlFor="formFile" className="form-label">Logotipo de la empresa</label>
+                    <input className="form-control" type="file" id="formFile" name="logotipo"/>
+                </div>
+                
+                <div className="row mb-3">
+                    <button type="submit" className="btn btn-primary" onClick={handleClick}>Registrar empresa</button>
+                </div>
+            </form>
+      </>
+        
       );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import Swal from 'sweetalert2'
 import {useNavigate} from 'react-router-dom'
+
 
 
 function Home() {
@@ -11,6 +13,14 @@ function Home() {
     }
 
     const handleClickRedux = ()=>{
+      Swal.fire({
+        title: 'Sweet!',
+        text: 'Modal with a custom image.',
+        imageUrl: 'https://unsplash.it/400/200',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+      })
         navigate('/redux')
     }
 
